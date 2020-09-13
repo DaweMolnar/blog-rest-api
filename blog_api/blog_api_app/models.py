e2812fcf101b9c91a04442b20735ed72260176bb
unknown <dawemolnar@gmail.com>
2020-09-13 11:43:17 +0100

    Initial commit with models

---
+++ blog_api/blog_api_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    profile_img_url = models.URLField()
+    registration_date = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=120, unique=True)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    up_votes = models.PositiveIntegerField(default=0)
+    down_votes = models.PositiveIntegerField(default=0)
+    labels = models.ManyToManyField(Label, related_name='posts')
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=30)
+    body = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    up_votes = models.PositiveIntegerField(default=0)
+    down_votes = models.PositiveIntegerField(default=0)
+
+
